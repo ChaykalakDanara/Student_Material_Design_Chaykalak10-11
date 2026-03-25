1361c6fe03f38c465dd0ca76a4b28c89f5575cf2
ВашÐcokes238 <_cokes238@gmail.com>
2026-03-25 09:33:45 +0300

    Step 7: Dark theme support added

--- app/src/main/java/com/timonin/student_material_design_Chaykalak/MainActivity.kt
+++ app/src/main/java/com/timonin/student_material_design_Chaykalak/MainActivity.kt
@@ -131,3 +131,10 @@ fun StudentPreview() {
         StudentApp()
     }
 }
+@Preview
+@Composable
+fun StudentDarkThemePreview(){
+    Student_Material_DesignTheme(darkTheme = true) {
+        StudentApp()
+    }
+}
