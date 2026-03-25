65043edb38b7b53bcd8439e685bf1b0a2f0f8e6d
ВашÐcokes238 <_cokes238@gmail.com>
2026-03-25 10:07:07 +0300

    Step 9: Custom typography added

--- app/src/main/java/com/timonin/student_material_design_Chaykalak/ui/theme/Type.kt
+++ app/src/main/java/com/timonin/student_material_design_Chaykalak/ui/theme/Type.kt
@@ -2,19 +2,28 @@ package com.timonin.student_material_design_Chaykalak.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.timonin.student_material_design_Chaykalak.R
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
+val Abrilfatface = FontFamily(
+    Font(R.font.abril_fatface_regular)
+)
+val Montserrat = FontFamily(
+    Font(R.font.abril_fatface_regular),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
+)
+//val Typography = Typography(
+//    bodyLarge = TextStyle(
+//        fontFamily = FontFamily.Default,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 16.sp,
+//        lineHeight = 24.sp,
+//        letterSpacing = 0.5.sp
+//    )
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -31,4 +40,26 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+//)
+val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = Abrilfatface,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    )
 )
